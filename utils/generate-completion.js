5db1fedb49636af2e6c94ac71fe8368ad477c53c
Devloper Zeroone <dev01@Devlopers-MacBook-Pro.local>
2024-07-25 09:30:07 +0700

    fixed

--- utils/generate-completion.js
+++ utils/generate-completion.js
@@ -31,13 +31,13 @@ const generateCompletion = async ({
   if (config.APP_ENV !== 'production') return new Completion({ text: MOCK_TEXT_OK });
   //const { data } = await createChatCompletion({ messages: prompt.messages });
   //console.log(prompt)
-  const responseText = await createThreadAndSendMessage({ assistantId: 'asst_TmjUbkjJWRc3AdAKUJN9wWhO', initialMessage: prompt });
+  const responseText = await createThreadAndSendMessage({ assistantId: 'asst_lGq4I4ZmSxB90OmdfGyulXwt', initialMessage: prompt });
   //console.log(responseText);
   //const [choice] = data.choices;
   return new Completion({
     text: responseText.trim(),
     //finishReason: choice.finish_reason,
-    finishReason: true,
+    finishReason: false,
   });
 };
 
